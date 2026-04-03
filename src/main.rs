bc329ea8a80d38864a28598b5842d298ffd9ddc5
Boxin Liu <boxin.liu@avanade.com>
2026-04-03 14:00:13 +0900

    minimal rust kernel

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,20 @@
 /// cargo build --target thumbv7em-none-eabihf
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
+
 /// 这个函数将作为程序的入口点,并且永远不会返回，禁用名称重整
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
@@ -16,4 +27,3 @@ pub extern "C" fn _start() -> ! {
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
-
