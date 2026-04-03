d99dc019de969881d936d14987f2a3daca662606
Boxin Liu <boxin.liu@avanade.com>
2026-04-03 13:28:15 +0900

    freestanding executable rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+///rustup target add thumbv7em-none-eabihf
+/// cargo build --target thumbv7em-none-eabihf
+use core::panic::PanicInfo;
+
+/// 这个函数将作为程序的入口点,并且永远不会返回，禁用名称重整
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
+
+/// 这个函数将在 panic 时被调用
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
