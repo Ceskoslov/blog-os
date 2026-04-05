06fb0263f81a417760be70d1265fac3d21c0495e
Ceskoslov <56268425+Ceskoslov@users.noreply.github.com>
2026-04-05 23:39:56 +0900

    basic support for async/await impl

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,20 @@ use bootloader::{BootInfo, entry_point};
 use x86_64::structures::paging::PageTable;
 extern crate alloc;
 use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
+use blog_os::task::{Task, simple_executor::SimpleExecutor};
+use blog_os::task::keyboard;
+use blog_os::task::executor::Executor;
 
 entry_point!(kernel_main);
+
+async fn async_number() -> u32 {
+    42
+}
+
+async fn example_task() {
+    let number = async_number().await;
+    println!("async number: {}", number);
+}
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     use blog_os::memory;
@@ -27,25 +39,14 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
     allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
-    let heap_value = Box::new(42);
-    println!("heap_value at {:p}", heap_value);
-
-    // 创建动态大小向量
-    let mut vec = Vec::new();
-    for i in 0..500 {
-        vec.push(i);
-    }
-    println!("vec at {:p}", vec.as_slice());
-
-    // 创建引用计数向量，计数为 0 时释放
-    let reference_counted = Rc::new(vec![1, 2, 3]);
-    let cloned_reference = reference_counted.clone();
-    println!("current reference count is {}",Rc::strong_count(&cloned_reference));
-    core::mem::drop(reference_counted);
-    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
 
     #[cfg(test)]
     test_main();
+
+    let mut executor = Executor::new(); // new
+    executor.spawn(Task::new(example_task()));
+    executor.spawn(Task::new(keyboard::print_keypresses()));
+    executor.run();
 
     println!("It did not crash!");
     blog_os::hlt_loop();
