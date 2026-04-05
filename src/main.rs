37dbf1b519958597d299ec68cfaffa33cbcf028a
Ceskoslov <56268425+Ceskoslov@users.noreply.github.com>
2026-04-05 21:42:43 +0900

    memory paging impl

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,30 @@
 
 use core::panic::PanicInfo;
 use blog_os::println;
+use bootloader::{BootInfo, entry_point};
+use x86_64::structures::paging::PageTable;
 
-// static HELLO: &[u8] = b"Hello World!";
+entry_point!(kernel_main);
 
-/// 这个函数将作为程序的入口点,并且永远不会返回，禁用名称重整
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
-    println!("hello {}!", "world");
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use blog_os::memory;
+    use x86_64::{VirtAddr, structures::paging::Page};
+    use blog_os::memory::BootInfoFrameAllocator;
+
+    println!("Hello World{}", "!");
     blog_os::init();
 
-    use x86_64::registers::control::Cr3;
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
-    let (level_4_page_table, _) = Cr3::read();
-    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+    // 映射未使用的页
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
+    // 通过新的映射将字符串 `New!`  写到屏幕上。
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
     #[cfg(test)]
     test_main();
 
